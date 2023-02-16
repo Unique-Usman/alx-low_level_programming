11d22877adee297a10952531578494135ad539a4
Unique-Usman <usmanakinyemi202@gmail.com>
2023-02-16 08:06:19 -0800

    MADE SOME CHANGES TO THE Last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
 /**
  * main - write to the standard error
  * some text
+ * some text
  * Return - return 1
 */
-
 int main(void)
 {
         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
