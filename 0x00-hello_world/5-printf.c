ba90617d58071c1ab0c6642f0becb094b91c7782
Unique-Usman <usmanakinyemi202@gmail.com>
2023-02-16 06:53:20 -0800

    Added some changes to task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main: to print some strings
- * return: return 0
+ * main - to print some strings
+ * using betty style
+ * Return: return 0
 */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);	
+	return (0);
 }
