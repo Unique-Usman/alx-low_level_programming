38a4ebc175de5cb65f8b60788bf70781d5a0399e
Unique-Usman <usmanakinyemi202@gmail.com>
2023-02-21 11:31:23 -0800

    ADDED TASK 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print 10 times the alphabet
+*followed by new line
+*/
+
+void print_alphabet_x10(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+	
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(c[i]);
+		}
+		_putchar('\n');
+	}
+}
