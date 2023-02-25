05d7cdcb2065afae11f69e0af1cce4342734e3f9
Unique-Usman <usmanakinyemi202@gmail.com>
2023-02-25 02:50:38 -0800

    Added some changes to the first taks k

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *_isupper - it checks if a character is upper or not
- *@c: the character to be check
- *Return: return 1 if c is upper 0 otherwise
+ * _isupper - it checks if a character is upper or not
+ * @c: the character to be check
+ * Return: return 1 if c is upper 0 otherwise
  */
-
 int _isupper(int c)
 {
 	return (c >= 65 && c<= 90);
