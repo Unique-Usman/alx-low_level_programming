f8bedb05e3ae3307ab88e2273370e2e2abd35bc5
Unique-Usman <usmanakinyemi202@gmail.com>
2023-02-25 03:18:56 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from zero to one
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 48 + 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
