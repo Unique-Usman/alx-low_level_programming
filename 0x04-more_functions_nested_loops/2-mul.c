db7c7c90b57f89fc6ba8362acf7cb31123756141
Unique-Usman <usmanakinyemi202@gmail.com>
2023-02-25 03:12:12 -0800

    Added the third task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - perform multiplication
+ * @a: one of the parameter
+ * @b: the secondparamter
+ * Return: return the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
