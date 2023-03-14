a23fb2e4aa1bbf1f900008b624e4888374659b0b
Unique-Usman <usmanakinyemi202@gmail.com>
2023-03-14 11:10:00 -0700

    made changes to task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,11 +12,14 @@ char *_strdup(char *str)
 	int i;
 	int count;
 	char *ptr;
+
 	if (str == NULL)
 		return (NULL);
 	while (*str != '\0')
 		count++;
 	ptr = malloc(count + 1);
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < count; i++)
 	{
 		ptr[i] = str[i];
