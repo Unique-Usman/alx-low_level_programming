b84ee62f34d22dffb821fe98b0999e2b855d51af
Unique-Usman <usmanakinyemi202@gmail.com>
2023-03-14 10:24:30 -0700

    Made some changes to task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (p);
 	}
-	p = malloc(sizeof(char) * c + 1);
+	p = malloc((sizeof(char) * c) + 1);
 	for (i = 0; i < (size - 1); i++)
 	{
 		p[i] = c;
