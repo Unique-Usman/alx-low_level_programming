d161d0c8e96e4b612b44eb45ff9b25b9223122c8
Unique-Usman <usmanakinyemi202@gmail.com>
2023-03-31 15:29:42 -0700

    Added the main.h file and also added the first first file which contains functions to print a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0
+	list_h* tmp = h;
+
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+		{
+			printf("[%d] (nil)\n");
+		}else
+		{
+			printf("[%u] %s\n", tmp->len, tmp->str);
+		}
+		tmp = tmp->next;
+		count++;
+	}
+
+	return (count);
+}
+
