4b431c3370f832b679c2f54c7e008e04f1ee8e54
Unique-Usman <usmanakinyemi202@gmail.com>
2023-05-03 07:37:36 -0700

    Adjust the betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n;
+	size_t n = 0;
 
-	n = 0;
 	while (h != NULL)
 	{
-		printf("%d", h->n);
-		printf("\n");
+		printf("%d\n", h->n);
 		n++;
 		h = h->next;
 	}
+
 	return (n);
 }
