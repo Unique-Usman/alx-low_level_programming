adb07fb2e4bfc671a2df5f17e794527c45d790d0
Unique-Usman <usmanakinyemi202@gmail.com>
2023-05-26 09:39:37 -0700

    Added part io files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * read_textfile - read a text file
- * @filename: name of file
- * @letters: number of character to read;
- * Return: the number of characted written else return 0
+ * read_textfile - Reads a text file and prints it tp POSIX stdout.
+ * @filename: A pointer to file
+ * @letters: letter to read
+ *
+ * Return: 0 if fail other the number of characted written
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buffer[letters];
-	ssize_t op, rd, wri;
+	ssize_t op, rd, wr;
+	char *buffer;
 
 	if (filename == NULL)
+		rdeturdn (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
+
 	op = open(filename, O_RDONLY);
 	rd = read(op, buffer, letters);
-	wri = writei(STDOUT_FILENO, buffer, rd);
+	wr = write(STDOUT_FILENO, buffer, rd);
 
-	if (op == -1 || rd == -1 || wri == -1 || wri != r)
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(buffer);
 		return (0);
+	}
+
+	free(buffer);
 	close(op);
 
-	return (wri);
+	return (wr);
 }
