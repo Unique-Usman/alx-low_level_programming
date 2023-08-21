001905d177a4b0fcf7e05ccab09b8a85034c4bb9
Unique-Usman <usmanakinyemi202@gmail.com>
2023-08-21 23:03:15 +0530

    A function that node to a double linked list at the begining and end

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - Insert a node with a value at a specific index in a doubly linked list.
+ * @h: Pointer to the head of the doubly linked list.
+ * @idx: Index at which to insert the new node (0-based).
+ * @n: Value to store in the new node.
+ * Return: Pointer to the new node, or NULL on failure.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *new_node, *current;
+
+    if (h == NULL)
+        return NULL;
+
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->n = n;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+
+    if (idx == 0)
+    {
+        if (*h != NULL)
+        {
+            new_node->next = *h;
+            (*h)->prev = new_node;
+        }
+        *h = new_node;
+        return new_node;
+    }
+
+    current = *h;
+    while (idx > 1 && current != NULL)
+    {
+        current = current->next;
+        idx--;
+    }
+
+    if (current == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    new_node->next = current->next;
+    new_node->prev = current;
+    if (current->next != NULL)
+        current->next->prev = new_node;
+    current->next = new_node;
+
+    return new_node;
+}
+
