91d41466643589cb3dedb46570040a52ba3266c7
Unique-Usman <usmanakinyemi202@gmail.com>
2023-08-22 07:28:33 +0530

    Changes the name of the library in the bash script to liball.so

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -fPIC -c *.c
-gcc -shared -o libdynamic.so *.o
-sudo cp libdynamic.so /usr/lib
+gcc -shared -o liball.so *.o
+sudo cp liball.so /usr/lib
 sudo ldconfig
