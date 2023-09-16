673e3594604cf7ef74f34bf484a06d21c6534b91
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-17 01:56:26 +0530

    Delete all the allocated memory

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,23 +8,27 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *tmp;
+	hash_table_t *head;
 	int i;
-	if (ht == NULL)
-		return;
+
+	head = ht;
 
 	for (i = 0; i < (int)ht->size; i++)
 	{
-		node = (ht->array)[i];
-
-		while (node != NULL)
+		if (ht->array[i] != NULL)
 		{
-			tmp = node->next;
-			free(node->value);
-			free(node->key);
-			free(node);
-			node = tmp;
+			node = (ht->array)[i];
+
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->value);
+				free(node->key);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
