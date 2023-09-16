f9ddc6ae3eff17c18ab56dcf818b4e64402df60b
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-16 22:26:03 +0530

    Handles malloc return well

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash;
 
 	hash = malloc(sizeof(hash_table_t));
+	if (hash == NULL)
+		return (NULL);
 	hash->size = size;
 	hash->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (hash == NULL || hash->array == NULL)
+	if (hash->array == NULL)
 		return (NULL);
 	return (hash);
 }
