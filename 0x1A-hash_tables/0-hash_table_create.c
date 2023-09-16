e642aa0fa2e654f9acc4045f1418fe00a523414a
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-16 22:22:04 +0530

    Create a folder for HashTable in C, also added a function that create a hashtable and also added the implementation of djb2 algorithm

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the hashtable
+ * Return: null if error else hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+
+	hash = malloc(sizeof(hash_table_t));
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash == NULL || hash->array == NULL)
+		return (NULL);
+	return (hash);
+}
