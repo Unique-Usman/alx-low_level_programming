e06dfd02c48382ce51f54e30ebe66d01c1a2243e
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-17 01:42:06 +0530

    Fixed the memory allocation issue

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,20 +2,20 @@
 
 /**
  * hash_table_delete - delete the element in a table
- * @ht - the hashtable
+ * @ht: the hashtable
  * Return: none
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *tmp;
 	int i;
+	if (ht == NULL)
+		return;
 
 	for (i = 0; i < (int)ht->size; i++)
 	{
 		node = (ht->array)[i];
 
-		if (node == NULL)
-			continue;
 		while (node != NULL)
 		{
 			tmp = node->next;
